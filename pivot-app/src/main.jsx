0b1d2b24e49456edf59d5196e994f9d09d619e03
PescaroDB <147217522+PescaroDB@users.noreply.github.com>
2026-05-12 17:38:21 -0400

    Delete pivot-app directory

--- pivot-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import PivotApp from './PivotApp.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <PivotApp />
-  </StrictMode>,
-)
